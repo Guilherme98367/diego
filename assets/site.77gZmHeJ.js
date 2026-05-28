8f47150741ef957f686b06ddae081e679dd34b87
Guilherme (via Manus) <guilherme@example.com>
2026-05-28 01:42:30 +0000

    refactor: reconstrução total, unificação e organização do projeto Dokmos

---
+++ assets/site.77gZmHeJ.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e9294dde576ea00caf33b0c588fd1eba5676e3d0280794ef9612991703bd9dd3
+size 99500
---
+++ media/site/1782844b74-1734620708/phamily-600x-q90.webp 600w, https_/www.troa.fr/media/site/1782844b74-1734620708/phamily-1024x-q80.webp 1024w, https_/www.troa.fr/media/phamily-2250x-q90.ef.de
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:307547dba12657252e8552918d1ab8c4a278d4ef97f73d218ac804a1793f021f
+size 31022
